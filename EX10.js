a2c129c08d99b29886783f4c4ce603f1f7348774
Myiah Moore <myiahsade@gmail.com>
2018-07-30 15:36:53 -0500

    check in

---
+++ EX10.js
@@ -0,0 +1,16 @@
+var card1 = parseInt(prompt("Enter card 1:  "));
+var card2 = parseInt(prompt("Enter card 2:  "));
+var card3 = parseInt(prompt("Enter card 3:  "));
+
+if (card1+card2+card3 === 21)
+{
+    alert("BlackJack!")
+}
+else if (card1+card2+card3 > 21)
+{
+    alert("Bust!!!")
+}
+else if (card1+card2+card3 < 21)
+{
+alert("The Total is:" +card1+card2+card3)
+}
